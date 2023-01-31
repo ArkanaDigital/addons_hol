dd39fee4a171a2ff481b6ff7707072986356e1b8
Amirul <muh.amirul.muk@gmail.com>
2023-01-31 08:49:00 +0700

    [INIT] ark_crm

---
+++ ark_crm/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Arkana CRM Customization",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Customer Relationship Management",
+    "website": "https://arkana.co.id",
+    "author": "PT. Arkana Solusi Digital",
+    "depends": [
+        "crm",
+    ],
+    "data": [
+        # "views/view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
