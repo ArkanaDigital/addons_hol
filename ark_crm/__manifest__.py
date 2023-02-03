9f53ad9f20a269b85c81ffc723cc765fa03ccaad
Amirul <muh.amirul.muk@gmail.com>
2023-02-03 17:21:07 +0700

    [ADD] ark_crm: auto archive when last update > 60 days

--- ark_crm/__manifest__.py
+++ ark_crm/__manifest__.py
@@ -1,12 +1,12 @@
 {
     "name": "Arkana CRM Customization",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.2",
     "license": "AGPL-3",
     "category": "Customer Relationship Management",
     "website": "https://arkana.co.id",
     "author": "PT. Arkana Solusi Digital",
     "depends": [
-        "crm", "web_m2x_options", "crm_lead_funnel_type", "partner_contact_job_position", "partner_contact_department", "crm_lead_last_update_from_openchatter"
+        "crm", "web_m2x_options", "crm_lead_funnel_type", "partner_contact_job_position", "partner_contact_department", "crm_lead_last_update_from_openchatter", "crm_lead_auto_archieve", "crm_lead_day_on_stage"
     ],
     "data": [
         "views/view.xml",
--- ark_crm/models/model.py
+++ ark_crm/models/model.py
@@ -1,6 +1,7 @@
 from odoo import fields, models, api, SUPERUSER_ID
 from odoo.exceptions import ValidationError
 from logging import getLogger
+from datetime import datetime
 
 _logger = getLogger(__name__)
 
@@ -13,7 +14,7 @@ class CrmLead(models.Model):
     @api.constrains('tag_ids')
     def constrains_tag_ids(self):
         for rec in self:
-            if len(rec.tag_ids) < 2 and len(rec.tag_ids.mapped('color')) < 2:
+            if len(rec.tag_ids) < 2 or len(set(rec.tag_ids.mapped('color'))) < 2:
                 raise ValidationError('Tags need to be filled with at least 2 diffrent values')
 
     @api.depends("message_ids")
@@ -34,3 +35,50 @@ class CrmLead(models.Model):
                 )[0].create_date
             
             document.last_update_openchatter = last_update_openchatter
+            document.day_from_last_chatter = document._compute_day_from_last_chatter()
+            _logger.info(f'update id crm.lead {document.id}')
+
+    @api.multi
+    def _compute_day_from_last_chatter(self):
+        dt_now = datetime.now()
+        for document in self:
+            if document.last_update_openchatter:
+                last_update_openchatter = datetime.strptime(document.last_update_openchatter, "%Y-%m-%d %H:%M:%S")
+                day_from_last_chatter = abs((dt_now - last_update_openchatter).days)
+            else:
+                day_from_last_chatter = 0
+            return day_from_last_chatter
+            
+    day_from_last_chatter = fields.Float(
+        string="Day From Last Update Openchatter",
+        compute="_compute_last_update_openchatter",
+        store=True, index=True
+    )
+
+
+class CrmTeam(models.Model):
+    _inherit = 'crm.team'
+
+    @api.multi
+    def _auto_archieve(self):
+        self.ensure_one()
+        obj_crm_lead = self.env["crm.lead"]
+        for auto_archieve in self.stage_auto_archieve_ids:
+            criteria = [
+                ("stage_id", "=", auto_archieve.stage_id.id),
+                ("active", "=", True),
+                '|', ("day_on_stage", ">", auto_archieve.day_limit),
+                ("day_from_last_chatter", ">", auto_archieve.day_limit_openchatter),
+            ]
+            leads = obj_crm_lead.search(criteria)
+            if leads:
+                for lead in leads:
+                    lead.write({"active": False})
+
+
+class CRMStageAutoArchieve(models.Model):
+    _inherit = "crm.stage_auto_archieve"
+
+    day_limit_openchatter = fields.Float(
+        string="Day Limit Openchatter", default=60
+    )
